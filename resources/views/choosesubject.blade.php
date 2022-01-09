ce6f4a50df4e5b286515241368c971414a6e5da3
Mallika-Dey <mallikaju307@gmail.com>
2022-01-09 18:56:13 +0600

    modify choosesubjectcss

--- resources/views/choosesubject.blade.php
+++ resources/views/choosesubject.blade.php
@@ -17,11 +17,15 @@
     <title>Choose Subject</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{asset('css/choosesubject.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=The+Nautigal:wght@700&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <!-- choose suject for student-->
     <!-- <h4></h4> -->
+    <h5 class="title">University Admission</h5>
     <div class="border border-5">
     	<p class="h4">Subject Choice Form</p>
         <form method="POST" action="{{ route('choose.submit') }}">
